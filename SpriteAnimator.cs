47869b68857f20a0dee42481bb7316f80aa44bfd
Ahmad Sajid <ahmad.sajid605@gmail.com>
2023-02-04 22:56:30 +0500

    Add files via upload

---
+++ SpriteAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frameArray;
+    private int currentFrame;
+    private float timer;
+    private float frameRate = 0.1f;
+    private SpriteRenderer _spriteRennderer;
+
+    private void Awake()
+    {
+        _spriteRennderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= frameRate)
+        {
+            timer -= frameRate;
+            currentFrame = (currentFrame + 1) % frameArray.Length;
+            _spriteRennderer.sprite = frameArray[currentFrame];
+        }
+    }
+}
