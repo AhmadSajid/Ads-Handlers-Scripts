c809cab8503ab2c8d1e7661cccf8d1d8324bc540
Ahmad Sajid <ahmad.sajid605@gmail.com>
2023-03-14 22:07:02 +0500

    Add files via upload

---
+++ PlayerAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetFalse()
+    {
+        GetComponent<Animator>().SetBool("idle", false);
+        GetComponent<Animator>().SetBool("run", false);
+        GetComponent<Animator>().SetBool("jump", false);
+        GetComponent<Animator>().SetBool("shoot", false);
+        GetComponent<Animator>().SetBool("dance", false);
+        GetComponent<Animator>().SetBool("death", false);
+    }
+
+    public void SetTrue(string _string)
+    {
+        GetComponent<Animator>().SetBool(_string, true);
+    }
+}
